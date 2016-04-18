e9ecfd2fac17b92e030882a5363738e8ca4ff711
desak29 <desak29@icloud.com>
2016-04-18 14:33:38 -0400

    made changes to add node routing for different web pages

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 var http = require('http');
-console.log(http);
-var server= http.createServer(function(request, response){
+var fs = require('fs');
 
-    response.writeHead(200,{'content-type':'text/plain'});
-    response.write('hello, world');
+
+function renderHomePage(request,response){
+    var homePageHtml = fs.readFileSync('homePage.html');
+    response.writeHead(200,{'content-type':'text/html'});
+        response.write(homePageHtml);
+    response.end();
+    }
+
+function renderNoPage(request,response){
+response.writeHead(404);
+    response.end('404,page not found');
+}
+
+
+
+var server= http.createServer(function(request, response){
+    if(request.url =='/'){
+        renderHomePage(request, response);
+    }else {
+        renderNoPage(request, response);
+    }
+    console.log('this is a line to show that some came to the server.');
     response.end();
 //    method createServer, takes two parameters a single call back
 });
 server.listen(8000);
-console.log("our server is listening on port 8000")
+console.log("our server is listening on port 8000.");
