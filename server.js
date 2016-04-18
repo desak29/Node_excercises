f7ccc49043de4b19885ed0d542ca833664eab695
desak29 <desak29@icloud.com>
2016-04-18 10:35:40 -0400

    created server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ console.log(http);
 var server= http.createServer(function(request, response){
 
     response.writeHead(200,{'content-type':'text/plain'});
-    response.end(0);
+    response.write('hello, world');
+    response.end();
 //    method createServer, takes two parameters a single call back
 });
 server.listen(8000);
